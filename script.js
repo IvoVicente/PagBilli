16beb4a1df3f231094fed234e785deb66d9543b4
Ivo Vicente <vicenten.junior@unifacol.edu.br>
2025-06-25 12:23:59 -0300

    jeitei o menu do celular

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    const toggles = document.querySelectorAll('.mobile-dropdown');
 
-    toggles.forEach(drop => {
-        drop.addEventListener('click', () => {
-            drop.classList.toggle('open');
-        });
-    });
-
-    const menuToggle = document.getElementById('menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
-
-    menuToggle.addEventListener('click', () => {
-        mobileMenu.classList.toggle('show');
-    });
-});
 
 // Abrir e fechar os dropdowns no mobile
 document.querySelectorAll(".mobile-dropdown > span").forEach(span => {
